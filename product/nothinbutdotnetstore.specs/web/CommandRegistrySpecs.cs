6c069c358da38d968d084f587b24c46fa7b867cd
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-14 12:11:18 -0400

    Added the second test

--- product/nothinbutdotnetstore.specs/web/CommandRegistrySpecs.cs
+++ product/nothinbutdotnetstore.specs/web/CommandRegistrySpecs.cs
@@ -45,5 +45,28 @@ namespace nothinbutdotnetstore.specs.web
              static Request request;
              static IList<WebCommand> all_web_commands;
          }
+
+         [Subject(typeof(DefaultCommandRegistry))]
+         public class when_attempting_to_get_a_command_for_a_request_and_it_does_not_have_the_command : concern
+         {
+
+             Establish c = () =>
+             {
+                 all_web_commands = new List<WebCommand>();
+                 provide_a_basic_sut_constructor_argument<IEnumerable<WebCommand>>(all_web_commands);
+                 request = an<Request>();
+             };
+
+             Because b = () =>
+                 result = sut.get_command_that_can_handle(request);
+
+
+             It should_return_the_command_that_can_handle_the_request = () =>
+                 result.ShouldBeAn<MissingWebCommand>();
+
+             static WebCommand result;
+             static Request request;
+             static IList<WebCommand> all_web_commands;
+         }
      }
  }
