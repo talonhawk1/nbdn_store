cbfdeeb43de02e44545a329df107d866cb2d8f7d
unknown <ZauggP@.science.regn.net>
2010-07-14 10:15:01 -0600

    passes test

--- product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore.web.core
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
-        public WebCommand get_command_that_can_handle(Request request)
+    	private readonly IEnumerable<WebCommand> _commands;
+
+    	public WebCommand get_command_that_can_handle(Request request)
         {
-            throw new NotImplementedException();
+            return (from c in _commands
+							 where c.can_handle(request)
+							 select c).
+    		Single();
         }
+	  public DefaultCommandRegistry(IEnumerable<WebCommand> commands)
+	  {
+	  	_commands = commands;
+	  }
     }
 }
