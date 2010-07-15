59f15a1d9eadb3747957825d3e7d442b8d3a47e9
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-15 16:49:56 -0400

    Somebody needs to add a check for a unary expression

--- product/nothinbutdotnetstore.specs/web/UrlHelperSpecs.cs
+++ product/nothinbutdotnetstore.specs/web/UrlHelperSpecs.cs
@@ -2,25 +2,26 @@ using Machine.Specifications;
 using nothinbutdotnetstore.model;
 using nothinbutdotnetstore.web.application;
 using nothinbutdotnetstore.web.core.helpers;
-using Machine.Specifications.DevelopWithPassion.Extensions;
 
 namespace nothinbutdotnetstore.specs.web
 {
     public class UrlHelperSpecs
     {
-        public abstract class concern {}
+        public abstract class concern
+        {
+        }
 
-        [Subject(typeof (Url))]
+        [Subject(typeof(Url))]
         public class when_creating_a_url_for_a_command : concern
         {
             Establish e = () =>
                 id = 42;
 
             Because b = () =>
-                result = Url.for_command<ViewSubDepartments,Department>(x => x.id = id);
+                result = Url.for_command<ViewSubDepartments, Department>(x => x.id);
 
             It should_return_url_with_command_name_with_store_extension_with_params_in_query_string = () =>
-                result.ShouldEqual(string.Format("ViewSubDepartments.store?id={0}",id));
+                result.ShouldEqual(string.Format("ViewSubDepartments.store?{0}", "id"));
 
             static int id;
             static string result;
--- product/nothinbutdotnetstore/web/core/helpers/Url.cs
+++ product/nothinbutdotnetstore/web/core/helpers/Url.cs
@@ -1,18 +1,23 @@
 using System;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace nothinbutdotnetstore.web.core.helpers
 {
     public class Url
     {
-        public static string for_command<T, T1>(params Func<T1, object>[] property_setters)
+        public static string for_command<T, T1>(params Expression<Func<T1,object>>[] property_setters)
         {
             var url = new StringBuilder(string.Format("{0}.store", typeof (T).Name));
+
+
             foreach (var property_setter in property_setters)
             {
+                var name = ((MemberExpression) property_setter.Body).Member.Name;
                 if (url.ToString().EndsWith(".store"))
                 {
-                    url.Append("?");
+                    url.AppendFormat("?{0}", name);
+
                 }
             }
             return url.ToString();
