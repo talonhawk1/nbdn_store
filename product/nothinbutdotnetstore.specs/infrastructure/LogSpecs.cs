d20784504543ba9a54343c8a5688b381e38d0dc3
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-07-16 13:34:24 -0400

    Removed the static initialization field on the Log class

--- product/nothinbutdotnetstore.specs/infrastructure/LogSpecs.cs
+++ product/nothinbutdotnetstore.specs/infrastructure/LogSpecs.cs
@@ -2,6 +2,7 @@
 using System;
 using Machine.Specifications;
 using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure.containers;
 using nothinbutdotnetstore.infrastructure.logging;
 using Rhino.Mocks;
 
@@ -21,9 +22,11 @@ namespace nothinbutdotnetstore.specs.infrastructure
             {
                 logger = an<Logger>();
                 logger_factory = an<LoggerFactory>();
+                container = an<Container>();
+                container.Stub(x => x.an_instance_of<LoggerFactory>()).Return(logger_factory);
 
-
-                resolver = () => logger_factory;
+                Func<Container> resolver = () => container;
+                change(() => IOC.factory_resolver).to(resolver);
 
                 logger_factory.Stub(x => x.get_logger_for(typeof(when_providing_access_to_the_underlying_logging_api))).Return(logger);
 
@@ -38,7 +41,7 @@ namespace nothinbutdotnetstore.specs.infrastructure
             static Logger result;
             static Logger logger;
             static LoggerFactory logger_factory;
-            static Func<LoggerFactory> resolver;
+            static Container container;
         }
     }
 }
