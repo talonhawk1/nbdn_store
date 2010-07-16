6522012ef3ae39c330019de7512686fd2fc6a21d
Jason Smale <smale.j@goldstandard.com>
2010-07-16 10:10:39 -0400

    container tests

--- product/nothinbutdotnetstore/infrastructure/containers/basic/BasicContainer.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/BasicContainer.cs
@@ -1,10 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class BasicContainer : Container
     {
+        readonly IDictionary<Type, DependencyFactory> factories;
+
+        public BasicContainer(IDictionary<Type, DependencyFactory> factories)
+        {
+            this.factories = factories;
+        }
+
         public Dependency an_instance_of<Dependency>()
+        {
+            var type = typeof (Dependency);
+            if (factories.ContainsKey(type))
+            {
+                return (Dependency) factories[type]();
+            }
+
+            throw new DependencyFactoryNotRegisteredException(type);
+        }
+
+        public void register_instance<Dependency>(Dependency instance)
         {
             throw new NotImplementedException();
         }
--- product/nothinbutdotnetstore/infrastructure/containers/basic/DependencyFactoryNotRegisteredException.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/DependencyFactoryNotRegisteredException.cs
@@ -4,6 +4,11 @@ namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class DependencyFactoryNotRegisteredException : Exception
     {
+        public DependencyFactoryNotRegisteredException(Type type_that_has_no_factory)
+        {
+            this.type_that_has_no_factory = type_that_has_no_factory;
+        }
+
         public Type type_that_has_no_factory { get; private set; }
     }
 }
