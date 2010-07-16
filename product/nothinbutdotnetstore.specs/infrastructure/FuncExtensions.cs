34fc79d7aeb102771c057555c50d464cdffdc331
Jason Smale <smale.j@goldstandard.com>
2010-07-16 12:54:47 -0400

    cache func extension

--- product/nothinbutdotnetstore.specs/infrastructure/FuncExtensions.cs
+++ product/nothinbutdotnetstore.specs/infrastructure/FuncExtensions.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.specs.infrastructure
 {
     public static class FuncExtensions
     {
+        static IDictionary<Func<object>, object> cache = new Dictionary<Func<object>, object>();
+
         public static Func<object> cache_result(this Func<object> factory)
         {
-            return factory;
+            return () => cache.ContainsKey(factory)
+                ? cache[factory]
+                : (cache[factory] = factory());
         }
     }
 }
